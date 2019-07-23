c99f36ce4b4ac87987c6c67602670625f888abcc
thondery <thondery@163.com>
2019-07-23 10:14:49 +0800

    remove lock & dist .

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_1 = require("./socket");
-exports.Socket = socket_1.default;
-var protobuffer_1 = require("./protobuffer");
-exports.Protobuffer = protobuffer_1.default;
